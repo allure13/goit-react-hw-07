749124dca14903efa4ba7489939bc616d4b33b54
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-03-27 22:50:19 +0200

    додав операції

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,31 +4,37 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
+    loading: false,
+    error: null,
   },
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.items.push(action.payload);
-      },
-      prepare(contact) {
-        return {
-          payload: {
-            id: nanoid(),
-            ...contact,
-          },
-        };
-      },
-    },
-
-    deleteContact(state, action) {
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload
-      );
-      state.items.splice(index, 1);
-    },
+  filters: {
+    name: '',
   },
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
 export const selectContacts = state => state.contacts.items;
+
+// reducers: {
+//   addContact: {
+//     reducer(state, action) {
+//       state.items.push(action.payload);
+//     },
+//     prepare(contact) {
+//       return {
+//         payload: {
+//           id: nanoid(),
+//           ...contact,
+//         },
+//       };
+//     },
+//   },
+
+//   deleteContact(state, action) {
+//     const index = state.items.findIndex(
+//       contact => contact.id === action.payload
+//     );
+//     state.items.splice(index, 1);
+//   },
+// },
